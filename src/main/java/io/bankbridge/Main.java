a66497d3cc9fd489a27b5b42b4e871526784e35e
Roar Thronæs <roart@pvv.ntnu.no>
2018-11-19 11:53:22 +0100

    Add support for running multiple servers, plus a method for stopping.

--- src/main/java/io/bankbridge/Main.java
+++ src/main/java/io/bankbridge/Main.java
@@ -1,20 +1,27 @@
 package io.bankbridge;
-import static spark.Spark.get;
-import static spark.Spark.port;
-
 import io.bankbridge.handler.BanksCacheBased;
 import io.bankbridge.handler.BanksRemoteCalls;
+import spark.Service;
 
 public class Main {
-
+    
+        // We need this to be able to run multiple servers
+        private static Service http = null;
+    
 	public static void main(String[] args) throws Exception {
-		
-		port(8080);
-
+	        http = Service.ignite();
+		http.port(8080);
 		BanksCacheBased.init();
 		BanksRemoteCalls.init();
 		
-		get("/v1/banks/all", (request, response) -> BanksCacheBased.handle(request, response));
-		get("/v2/banks/all", (request, response) -> BanksRemoteCalls.handle(request, response));
+		http.get("/v1/banks/all", (request, response) -> BanksCacheBased.handle(request, response));
+		http.get("/v2/banks/all", (request, response) -> BanksRemoteCalls.handle(request, response));
 	}
+
+    // We need this to make sure the service is stopped at the end of each test
+    public static void stop() {
+        if (http != null) {
+            http.stop();
+        }
+    }
 }
--- src/test/java/io/bankbridge/MockRemotes.java
+++ src/test/java/io/bankbridge/MockRemotes.java
@@ -2,26 +2,32 @@ package io.bankbridge;
 import static spark.Spark.get;
 import static spark.Spark.port;
 
+import spark.Service;
+
 public class MockRemotes {
 
-	public static void main(String[] args) throws Exception {
-		
-		port(1234);
+    public static void main(String[] args) throws Exception {
+        Service http = Service.ignite();
 
-		get("/rbb", (request, response) -> "{\n" + 
-				"\"bic\":\"1234\",\n" + 
-				"\"countryCode\":\"GB\",\n" + 
-				"\"auth\":\"OAUTH\"\n" + 
-				"}");
-		get("/cs", (request, response) -> "{\n" + 
-				"\"bic\":\"5678\",\n" + 
-				"\"countryCode\":\"CH\",\n" + 
-				"\"auth\":\"OpenID\"\n" + 
-				"}");
-		get("/bes", (request, response) -> "{\n" + 
-				"\"name\":\"Banco de espiritu santo\",\n" + 
-				"\"countryCode\":\"PT\",\n" + 
-				"\"auth\":\"SSL\"\n" + 
-				"}");
-	}
+        http.port(1234);
+
+        http.get("/rbb", (request, response) -> "{\n" + 
+                "\"bic\":\"1234\",\n" + 
+                "\"name\":\"Royal Bank of Boredom\",\n" +
+                "\"countryCode\":\"GB\",\n" + 
+                "\"auth\":\"OAUTH\"\n" + 
+                "}");
+        http.get("/cs", (request, response) -> "{\n" + 
+                "\"bic\":\"5678\",\n" + 
+                "\"name\":\"Credit Sweets\",\n" +
+                "\"countryCode\":\"CH\",\n" + 
+                "\"auth\":\"OpenID\"\n" + 
+                "}");
+        http.get("/bes", (request, response) -> "{\n" + 
+                "\"bic\":\"9870\",\n" + 
+                "\"name\":\"Banco de espiritu santo\",\n" + 
+                "\"countryCode\":\"PT\",\n" + 
+                "\"auth\":\"SSL\"\n" + 
+                "}");
+    }
 }
